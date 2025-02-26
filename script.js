d1f75a105f14c5e3fbdca38ef7bfe388d900c682
papudepapus <113124999+papudepapus@users.noreply.github.com>
2025-02-26 19:16:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 30,
+    grabCursos:true,
+    navigation: {
+        nextEl: '.swiper-button-next', 
+        prevEl: '.swiper-button-prev' 
+    },
+    breakpoints: {
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
